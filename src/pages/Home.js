a842b454316eecc720368a2566920166bf9cce81
Shaik Ahamd Nawaz <nawazcode@gmail.com>
2022-09-03 00:40:17 +0530

    unique Id

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
 // rafce - react arrow function component export
-import React from "react";
-
+import React, { useState } from "react";
+import { v4 as uuidV4 } from "uuid";
 const Home = () => {
+  const [roomId, setRoomId] = useState("");
+  const [userName, setUserName] = useState("");
+  const createNewRoom = (e) => {
+    // preventDefault is used to prevent refreshing
+    //the page by clicking < a > tab
+    e.preventDefault();
+    const id = uuidV4();
+    setRoomId(id);
+  };
   return (
     <div className="homePageWrapper">
       <div className="formWrapper">
@@ -12,14 +21,27 @@ const Home = () => {
         />
         <h4 className="mainLabel">Paste invitation ROOM ID</h4>
         <div className="inputGroup">
-          <input type="text" className="inputBox" placeholder="ROOM ID" />
-          <input type="text" className="inputBox" placeholder="USER NAME" />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="ROOM ID"
+            // we can change Id manually by doing like this
+            onChange={(e) => setRoomId(e.target.value)}
+            value={roomId}
+          />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="USER NAME"
+            onChange={(e) => setUserName(e.target.value)}
+            value={userName}
+          />
           <button className="btn joinBtn">Join</button>
           <span className="createInfo">
             {/* non-breaking space: &nbsp; A non-breaking space is a space 
                 that will not break into a new line. */}
             If you don't have an invite then create&nbsp;
-            <a href="newRoom" className="createNewBtn">
+            <a onClick={createNewRoom} href="newRoom" className="createNewBtn">
               new room
             </a>
           </span>
